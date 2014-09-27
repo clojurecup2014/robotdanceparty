78bff8b5d7e347ea846b4a9f30d2eeb1a2db0623
Kris C <kriscalabio@gmail.com>
2014-09-27 14:34:51 +0000

    robot recorded!

--- src/robotdanceparty/core.cljs
+++ src/robotdanceparty/core.cljs
@@ -11,6 +11,10 @@
 (def game #js{})
 (def ticker (atom 0))
 
+(def robot #js {})
+
+
+
 ; define game.setup()
 (aset game "setup"
   (fn []
@@ -27,6 +31,12 @@
                                  "scale" 2
                                  "anchor" "bottom_center"}))
 
+      (set! robot (.Sprite js/jaws
+                            #js {"x" 100
+                                 "y" 100
+                                 "scale" 2
+                                 "anchor" "bottom_center"}))
+
       ; setup player animations
       (aset player "anim_default" (.slice anim 0 3))
       (aset player "anim_up" (.slice anim 3 6))
@@ -34,13 +44,37 @@
       (aset player "anim_left" (.slice anim 3 6))
       (aset player "anim_right" (.slice anim 3 6))
 
+      (aset robot "anim_default" (.slice anim 0 3))
+      (aset robot "anim_up" (.slice anim 3 6))
+      (aset robot "anim_down" (.slice anim 3 6))
+      (aset robot "anim_left" (.slice anim 3 6))
+      (aset robot "anim_right" (.slice anim 3 6))
+
       ; setup player replay property
       (aset player "replay" {})
 
+
       (.setImage player (.next (aget player "anim_default")))
+
+      (.setImage robot (.next (aget robot "anim_default")))
+
       (.preventDefaultKeys js/jaws
                            #js ["up" "down" "left" "right" "space"]))))
-(println (aget player "replay"))
+
+(defn move [entity dir]
+  (case dir
+    "left" (do
+             (aset entity "x" (- (aget entity "x") speed))
+             (.setImage entity (.next (aget entity "anim_left"))))
+    "right"(do
+             (aset entity "x" (+ (aget entity "x") speed))
+             (.setImage entity (.next (aget entity "anim_right"))))
+    "up"   (do
+             (aset entity "y" (- (aget entity "y") speed))
+             (.setImage entity (.next (aget entity "anim_up"))))
+    "down" (do
+             (aset entity "y" (+ (aget entity "y") speed))
+             (.setImage entity (.next (aget entity "anim_down"))))))
 
 (defn replay-add [entity dir]
  (let [replay (aget entity "replay")]
@@ -54,6 +88,8 @@
                   (str @ticker)
                   #{dir})))))
 
+(print (aget player "replay"))
+
 ; define game.update()
 (aset game "update"
   (fn []
@@ -64,27 +100,23 @@
 
     (if (.pressed js/jaws "left")
         (do
-          (aset player "x" (- (aget player "x") speed))
-          (.setImage player (.next (aget player "anim_left")))
+          (move player "left")
           (replay-add player "left")
           (if-not (aget player "flipped")
             (.flip player))))
     (if (.pressed js/jaws "right")
         (do
-          (aset player "x" (+ (aget player "x") speed))
-          (.setImage player (.next (aget player "anim_right")))
+          (move player "right")
           (replay-add player "right")
           (if (aget player "flipped")
             (.flip player))))
     (if (.pressed js/jaws "up")
         (do
-          (aset player "y" (- (aget player "y") speed))
-          (.setImage player (.next (aget player "anim_up")))
+          (move player "up")
           (replay-add player "up")))
     (if (.pressed js/jaws "down")
         (do
-          (aset player "y" (+ (aget player "y") speed))
-          (.setImage player (.next (aget player "anim_down")))
+          (move player "down")
           (replay-add player "down")))
 
     (if (= 0 (mod @ticker 60))
@@ -95,14 +127,33 @@
                                  "/"
                                  (aget player "y")
                                  ". timer: "
-                                 (/ @ticker 60)))))))
+                                 (/ @ticker 60)))))
+
+    (if (aget robot "replay")
+      (if-let [tick ((aget robot "replay") (str @ticker))]
+        (move robot (first tick))))
+
+    (if (> @ticker 600)
+      (do
+        (reset! ticker 0)
+        (if-not (aget robot "replay")
+          (aset robot "replay"
+                (aget player "replay"))
+          ))
+      )
+    ))
+
+(aget robot "replay")
 
 ; define game.draw()
 (aset game "draw"
   (fn []
     (.clear js/jaws)
     (.log js/jaws player)
-    (.draw player)))
+    (if robot (.log js/jaws robot))
+    (.draw player)
+    (if robot (.draw robot))
+    ))
 
 ; define jaws.onload()
 (aset js/jaws "onload"
