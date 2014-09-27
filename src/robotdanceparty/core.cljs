dd467364ac19869e937aac5077ff0d1708e66057
Kris C <kriscalabio@gmail.com>
2014-09-27 13:33:08 +0000

    replays recorded

--- src/robotdanceparty/core.cljs
+++ src/robotdanceparty/core.cljs
@@ -9,6 +9,7 @@
 (def player #js {})
 (def speed 2)
 (def game #js{})
+(def ticker (atom 0))
 
 ; define game.setup()
 (aset game "setup"
@@ -32,13 +33,32 @@
       (aset player "anim_down" (.slice anim 3 6))
       (aset player "anim_left" (.slice anim 3 6))
       (aset player "anim_right" (.slice anim 3 6))
+
+      ; setup player replay property
+      (aset player "replay" {})
+
       (.setImage player (.next (aget player "anim_default")))
       (.preventDefaultKeys js/jaws
                            #js ["up" "down" "left" "right" "space"]))))
+(println (aget player "replay"))
+
+(defn replay-add [entity dir]
+ (let [replay (aget entity "replay")]
+   (if (replay (str @ticker))
+     (aset player "replay"
+           (assoc (aget entity "replay")
+                  (str @ticker)
+                  (conj (replay (str @ticker)) dir)))
+     (aset player "replay"
+           (assoc (aget player "replay")
+                  (str @ticker)
+                  #{dir})))))
 
 ; define game.update()
 (aset game "update"
   (fn []
+
+    (swap! ticker inc)
 
     (.setImage player (.next (aget player "anim_default")))
 
@@ -46,29 +66,36 @@
         (do
           (aset player "x" (- (aget player "x") speed))
           (.setImage player (.next (aget player "anim_left")))
+          (replay-add player "left")
           (if-not (aget player "flipped")
             (.flip player))))
     (if (.pressed js/jaws "right")
         (do
           (aset player "x" (+ (aget player "x") speed))
           (.setImage player (.next (aget player "anim_right")))
+          (replay-add player "right")
           (if (aget player "flipped")
             (.flip player))))
     (if (.pressed js/jaws "up")
         (do
           (aset player "y" (- (aget player "y") speed))
-          (.setImage player (.next (aget player "anim_up")))))
+          (.setImage player (.next (aget player "anim_up")))
+          (replay-add player "up")))
     (if (.pressed js/jaws "down")
         (do
           (aset player "y" (+ (aget player "y") speed))
-          (.setImage player (.next (aget player "anim_down")))))
+          (.setImage player (.next (aget player "anim_down")))
+          (replay-add player "down")))
 
-    (let [fps (.getElementById js/document "fps")]
-      (aset fps "innerHTML" (str (aget (aget js/jaws "game_loop") "fps")
+    (if (= 0 (mod @ticker 60))
+      (let [fps (.getElementById js/document "fps")]
+        (aset fps "innerHTML" (str (aget (aget js/jaws "game_loop") "fps")
                                  ". player: "
                                  (aget player "x")
                                  "/"
-                                 (aget player "y"))))))
+                                 (aget player "y")
+                                 ". timer: "
+                                 (/ @ticker 60)))))))
 
 ; define game.draw()
 (aset game "draw"
