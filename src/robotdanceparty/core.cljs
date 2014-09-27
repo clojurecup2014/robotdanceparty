56dec83e12b1374921167c17436d66c0560b5d13
Kris C <kriscalabio@gmail.com>
2014-09-27 12:20:25 +0000

    example droid guy walks around!

--- src/robotdanceparty/core.cljs
+++ src/robotdanceparty/core.cljs
@@ -5,8 +5,9 @@
 (defn ^:export main []
   (println "Hello, Clojure Cup!"))
 
-; 'declare' the game state object and player
+; 'declare' globals
 (def player #js {})
+(def speed 2)
 (def game #js{})
 
 ; define game.setup()
@@ -25,8 +26,47 @@
                                  "scale" 3
                                  "anchor" "bottom_center"}))
 
+      ; setup player animations
       (aset player "anim_default" (.slice anim 0 5))
-      (.setImage player (.next (aget player "anim_default"))))))
+      (aset player "anim_up" (.slice anim 6 8))
+      (aset player "anim_down" (.slice anim 8 10))
+      (aset player "anim_left" (.slice anim 10 12))
+      (aset player "anim_right" (.slice anim 12 14))
+      (.setImage player (.next (aget player "anim_default")))
+      (.preventDefaultKeys js/jaws
+                           #js ["up" "down" "left" "right" "space"]))))
+
+; define game.update()
+(aset game "update"
+  (fn []
+
+    (.setImage player (.next (aget player "anim_default")))
+
+    (if (.pressed js/jaws "left")
+        (do
+          (aset player "x" (- (aget player "x") speed))
+          (.setImage player (.next (aget player "anim_left")))))
+    (if (.pressed js/jaws "right")
+        (do
+          (aset player "x" (+ (aget player "x") speed))
+          (.setImage player (.next (aget player "anim_right")))))
+    (if (.pressed js/jaws "up")
+        (do
+          (aset player "y" (- (aget player "y") speed))
+          (.setImage player (.next (aget player "anim_up")))))
+    (if (.pressed js/jaws "down")
+        (do
+          (aset player "y" (+ (aget player "y") speed))
+          (.setImage player (.next (aget player "anim_down")))))
+
+
+    ;(.setImage player (.next (aget player "anim_default")))
+    (let [fps (.getElementById js/document "fps")]
+      (aset fps "innerHTML" (str (aget (aget js/jaws "game_loop") "fps")
+                                 ". player: "
+                                 (aget player "x")
+                                 "/"
+                                 (aget player "y"))))))
 
 ; define game.draw()
 (aset game "draw"
